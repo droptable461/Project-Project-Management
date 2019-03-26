cbbc6a9ff30ba06bb3f150692961f9710be86b0d
Thomas Bain <bainth@pascal.cs.longwood.edu>
2019-03-26 15:33:34 -0400

    -u "thomas bain" -c "edited database.py to make uname a primary key, continuing to work on app.py, connected login.html to app.py"

--- Website/app.py
+++ Website/app.py
@@ -1,34 +1,35 @@
 from flask import Flask, request, g, render_template, redirect, url_for, escape
 import sqlite3
 
-conn = sqlite3.connect('database.db')
+#conn = sqlite3.connect('database.db')
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-#@app.route('/')
-#def hello():
-    #return 'Hello, world! running on'
+@app.route('/')
+def hello():
+    return render_template("login.html")
 
 #def login():
-c = conn.cursor()
-username = input('username')
-c.execute("SELECT uname FROM user WHERE uname = '%s'" % (username))
-one = c.fetchall()
-if one is True:
-    return True 
-else:
-    return False
+#c = conn.cursor()
+#username = input('username')
+#c.execute("SELECT uname FROM user WHERE uname = '%s'" % (username))
+#one = c.fetchall()
+#if one is True:
+#    print True 
+#else:
+#    print False
 
-#@app.route('/login', methods=['POST'])
-#def login():
-    #c = conn.cursor()
-    #username = escape(request.form['uname'])
-    #c.execute("SELECT uname FROM user WHERE uname = '%s'" % (username))
-    #if fetchall() is true:
-            #return render_template("index.html", name = username)
-    #else:
-            #return render_template("login.html")
+@app.route('/login_q')#,methods=["POST"])
+def login():
+    with sqlite3.connect('database.db') as db:
+        db.cursor()
+        username = escape(request.form['Username'])
+        db.execute("SELECT uname FROM user WHERE uname = '%s'" % (username))
+        if db.fetchall() is true:
+                return render_template("index.html", name = username)
+        else:
+                return render_template("login.html")
         
 if __name__ == '__main__':
-    #app.run(debug=True)
+    app.run(debug=True)
     #login()s
