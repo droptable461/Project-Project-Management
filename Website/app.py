2a89c0c4afc3dab5cb87faf7b6e9080359ea6ffd
Thomas Bain <bainth@pippin.cs.longwood.edu>
2019-03-29 13:18:52 -0400

    -u "thomas b" -c "cleaned up app.py"

--- Website/app.py
+++ Website/app.py
@@ -19,47 +19,24 @@ def getDB():
 def hello():
     return render_template("login.html")
 
-<<<<<<< HEAD
-@app.route('/',methods=["POST"])
-def login():
-    with sqlite3.connect('database.db') as db:
-        c = db.cursor()
-        if "uname" in request.form:
-            username = escape(request.form["uname"])
-            print (username)
-            c.execute("SELECT uname FROM user WHERE uname=?",  (username,))
-            check = c.fetchone()
-            print (check)
-            if check == username:
-                    return render_template("index.html", name = username)
-            else:
-                    print ("failed")
-                    return render_template("login.html")
-=======
-#def login():
-#c = conn.cursor()
-#username = input('username')
-#c.execute("SELECT uname FROM user WHERE uname = '%s'" % (username))
-#one = c.fetchall()
-#if one is True:
-#    print True
-#else:
-#    print False
-
+#thomas version
 #@app.route('/',methods=["POST"])
 #def login():
-    #with sqlite3.connect('database.db') as db:
-        #db.cursor()
-        #if "uname" in request.form:
-            #username = escape(request.form["uname"])
-            #db.execute("SELECT uname FROM user WHERE uname = '%s'"% (username))
-            #check = db.fetchone() #tejas, the error im getting is here:
-                                  ##AttributeError: 'sqlite3.Connection' object has no attribute 'fetchone'
-                                  ## -thomas
-            #if check is true:
-                    #return render_template("index.html", name = username)
-        #return render_template("login.html")
+#    with sqlite3.connect('database.db') as db:
+#        c = db.cursor()
+#        if "uname" in request.form:
+#            username = escape(request.form["uname"])
+#            print (username)
+#           c.execute("SELECT uname FROM user WHERE uname=?",  (username,))
+#            check = c.fetchone()
+#            print (check)
+#            if check == username:
+#                    return render_template("index.html", name = username)
+#            else:
+#                    print ("failed")
+#                    return render_template("login.html")
 
+#tejas v
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
@@ -71,7 +48,7 @@ def login():
 
 		if validlogin:
 			session['username'] = request.form['username']
-			return redirect(url_for('index'))
+			return render_template("index.html")
 	return render_template('login.html')
 
 #@app.route('/myproj', methods=['GET', 'POST'])
@@ -87,7 +64,6 @@ def login():
 			#session['username'] = request.form['username']
 			#return redirect(url_for('index'))
 	#return render_template('login.html')
->>>>>>> f6e6c85cf8a3239eb14ed6024efd1d61c60288b9
 
 @app.route('/index')
 def index():
