da994845e3c08a7a61356aee9e665cf2578fc70b
Thomas Bain <bainth@chomsky.cs.longwood.edu>
2019-03-31 15:54:54 -0400

    -u "thomas b" -c "reformatting database.py to generate useful relations"

--- Website/database.py
+++ Website/database.py
@@ -7,14 +7,12 @@ def clearDatabase():
 	c.execute('''DROP TABLE IF EXISTS task''')
 	c.execute('''DROP TABLE IF EXISTS bug''')
 	c.execute('''DROP TABLE IF EXISTS user''')
-	c.execute('''DROP TABLE IF EXISTS uttable''')
 def createDatabase():
 	c = conn.cursor()
-	c.execute('''CREATE TABLE project(manager text, p_id serial, title text, description text)''')
-	c.execute('''CREATE TABLE task(task_id int PRIMARY KEY UNIQUE AUTOINCREMENT, title text, description text)''')
-	c.execute('''CREATE TABLE bug(b_id serial, t_id int, line int, fname text, description text)''')
-	c.execute('''CREATE TABLE user(uname text PRIMARY KEY, u_id)''')
-	c.execute('''CREATE TABLE uttable(u_id, t_id)''')
+	c.execute('''CREATE TABLE project(manager TEXT, title TEXT PRIMARY KEY UNIQUE, description TEXT, tID REFERENCES task(task_id))''')
+	c.execute('''CREATE TABLE task(task_id INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT, title TEXT, description TEXT, phase TEXT, bug_id REFERENCES bug(b_id))''')
+	c.execute('''CREATE TABLE bug(b_id PRIMARY KEY UNIQUE AUTOINCREMENT, line INTEGER, fname TEXT, description TEXT)''')
+	c.execute('''CREATE TABLE user(uname text PRIMARY KEY UNIQUE, proj TEXT REFERENCES project(title), tasks REFERENCES task(task_id))''')
 	conn.commit()
 def populateDatabase():
         c = conn.cursor()
