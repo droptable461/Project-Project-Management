75b8ace88d1f5b9a058c1ea8dc947d0af1ea8940
Thomas Bain <bainth@frodo.cs.longwood.edu>
2019-04-12 13:12:55 -0400

    -u "thomas b" -c "linkinh projects to web page"

--- Website/app.py
+++ Website/app.py
@@ -1,8 +1,5 @@
 from flask import Flask, request, g, render_template, redirect, url_for, escape, session
 import sqlite3
-import requests
-import simplejson as json
-
 
 #conn = sqlite3.connect('database.db')
 
@@ -22,29 +19,6 @@ def getDB():
         if not hasattr(g, 'sqlite_db'):
             g.sqlite_db = connectDB()
         return g.sqlite_db
-def addProj():
-	if 'manager' in request.form:
-	    db = getDB()
-	    man = request.form['manager']
-	    title = request.form['title']
-	    des = request.form['description']
-	    db.execute("""INSERT INTO project (manager,title,description) VALUES(?,?,?)""",(man,title,des))
-	    db.commit()
-	    db.close()
-	return
-
-def addTask():
-	if 'title2' in request.form:
-	    db1 = getDB()
-	    #taskID = request.form['task_id']
-	    taskTitle = request.form['title2']
-	    taskDes = request.form['description2']
-	    taskPhase = request.form['phase']
-	   # taskBug = request.form['bug_id']
-	    db1.execute("""INSERT INTO task (title,description,phase) VALUES(?,?,?)""",(taskTitle,taskDes,taskPhase))
-	    db1.commit()
-	    db1.close()
-	return
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -65,19 +39,27 @@ def hello():
 
 @app.route('/myproj', methods=['GET','POST'])
 def myproj():
+        c = getDB()
+        p = [row[0] for row in c.execute("""SELECT DISTINCT proj FROM user WHERE uname = (?)""",(session['username'],)).fetchall()]
+        #for j in range(len(p)):
+            #print(p[j])
 
-#dif func for dif proj operations: add(proj or tasks), retrieve(proj and tasks), remove(proj or tasks), modify(proj or tasks)
         if request.method == 'POST':
-             addProj()
-             addTask()
+            addproj()
 
-        return render_template('myproj.html')
+    return render_template('myproj.html')
+        
+#dif func for dif proj operations: add(proj or tasks), retrieve(proj and tasks), remove(proj or tasks), modify(proj or tasks)
+def addproj():
+    if request.method == 'POST':
+        db = getDB()
+        man = request.form['manager']
+        title = request.form['title']
+        des = request.form['description']
+        db.execute("INSERT INTO project (manager,title,description) VALUES(?,?,?)",(man,title,des))
+        db.commit()
+    return True
 
-@app.route('/Project1')
-def Project1():
-
-
-    return render_template('Project1.html')
 
 @app.route('/inbox')
 def inbox():
@@ -91,14 +73,10 @@ def howto():
 def index():
     return render_template('index.html')
 
-@app.route("/task", methods=['GET', 'POST'])
-def task():
-        req = requests.get('http://127.0.0.1:5000/task')
-        print(req.text)
-        #print("HTTP Status Code: " + str(req.status_code))
-        #print(req.headers)
-        #json_response = json.loads(req.content)
-        #print(json_response['t_description'])
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+    #request
+     return make_response('Test ..')
 
 @app.teardown_appcontext
 def closeDB(error):
