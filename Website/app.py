b83621a7673c208af9bddc21b2c78f9ca2ebe04c
Thomas Bain <bainth@frodo.cs.longwood.edu>
2019-04-15 12:54:01 -0400

    -u "thomas b" -c "fixing merges"

--- Website/app.py
+++ Website/app.py
@@ -39,6 +39,8 @@ def hello():
 
 @app.route('/myproj', methods=['GET','POST'])
 def myproj():
+        c = getDB()
+        p = [row[0] for row in c.execute("""SELECT DISTINCT proj FROM user WHERE uname = (?)""",(session['username'])).fetchall()]
 
 #dif func for dif proj operations: add(proj or tasks), retrieve(proj and tasks), remove(proj or tasks), modify(proj or tasks)
         if request.method == 'POST':
